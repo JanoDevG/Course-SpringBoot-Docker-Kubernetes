9d1dc9174111e9f8674c886a3193bc02597a6f8e
Alejandro Gutiérrez <janodevg@outlook.cl>
2023-03-06 12:13:23 -0300

    carga proyecto cutomers

---
+++ ms-customer/src/test/java/cl/janodevg/ms/mscustomer/MsCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.janodevg.ms.mscustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
