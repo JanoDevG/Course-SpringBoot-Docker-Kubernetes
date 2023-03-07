b9d9b03879cedd3ee3d5c4051557b1eed7f08aa9
Alejandro Gutiérrez <janodevg@outlook.cl>
2023-03-07 17:11:34 -0300

    creación nuevo ms

---
+++ ms-courses/src/test/java/cl/janodevg/ms/courses/mscourses/MsCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.janodevg.ms.courses.mscourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
